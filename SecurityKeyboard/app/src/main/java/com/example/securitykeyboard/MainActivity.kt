5f27c0cdd2fe96cad050930af223156bf55b2716
kgh <vot768@naver.com>
2021-11-17 13:36:25 +0900

    save

--- SecurityKeyboard/app/src/main/java/com/example/securitykeyboard/MainActivity.kt
+++ SecurityKeyboard/app/src/main/java/com/example/securitykeyboard/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.securitykeyboard
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
@@ -17,42 +18,42 @@ import java.lang.StringBuilder
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private val mActivity: Activity? = this
-    private var currentText: StringBuilder = StringBuilder()
-
     private lateinit var editText: EditText
     private lateinit var flipper: ViewFlipper
-    private lateinit var tablelayout: TableLayout
-    private lateinit var textView: TextView
-    private lateinit var imm: InputMethodManager
+    private lateinit var tableLayout: TableLayout
+    private lateinit var textView : TextView
+    private lateinit var inputmethonmanager: InputMethodManager
+
+    private val mActivity: Activity? = this
+    private var currentText : StringBuilder = StringBuilder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputmethonmanager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         val btnWidth: Int = getBtnWidth()
 
-        flipper = password_viewflipper.apply {
+        flipper = findViewById<ViewFlipper>(R.id.password_viewflipper).apply {
             visibility = View.VISIBLE
 
             var inAni = AnimationUtils.loadAnimation(mActivity, R.anim.in_animation)
-            inAnimation.interpolator = AccelerateInterpolator()
+            inAni.interpolator = AccelerateInterpolator()
             setInAnimation(inAni)
 
             var outAni = AnimationUtils.loadAnimation(mActivity, R.anim.out_animation)
-            outAnimation.interpolator = DecelerateInterpolator()
+            outAni.interpolator = DecelerateInterpolator()
             setOutAnimation(outAni)
         }
 
-        tablelayout = findViewById<TableLayout>(R.id.tableLayout)
-        textView = findViewById<TextView>(R.id.password_text_view)
+        tableLayout = findViewById<TableLayout>(R.id.tableLayout)
+        textView = findViewById<TextView>(R.id.password_text)
         val emptyView = TextView(this)
         flipper.addView(emptyView, 0)
 
-        editText = findViewById<EditText>(R.id.password_text_view).apply {
+        editText = findViewById<EditText>(R.id.inputpasswordlabel).apply {
             setOnClickListener { v: View ->
-                imm.hideSoftInputFromWindow(editText.windowToken, 0)
+                inputmethonmanager.hideSoftInputFromWindow(editText.windowToken, 0)
 
                 if(flipper.currentView.id != R.id.firstViewFlipper) {
                     reOrderKeyboard(btnWidth)
@@ -62,16 +63,16 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        imm.hideSoftInputFromWindow(editText.windowToken, 0)
+        inputmethonmanager.hideSoftInputFromWindow(editText.windowToken, 0)
         editText.requestFocus()
 
         findViewById<Button>(R.id.submitbtn).apply {
             setOnClickListener { v: View ->
                 if(flipper.currentView.id == R.id.firstViewFlipper) {
                     flipper.displayedChild = 0
-                    var showText = StringBuilder(textView.text)
-                    showText.append("\n" + currentText)
-                    textView.text = showText.toString()
+                    var showtxt = StringBuilder(textView.text)
+                    showtxt.append("\n" + currentText)
+                    textView.text = showtxt.toString()
                     currentText.clear()
                     editText.text.clear()
                 }
@@ -82,24 +83,24 @@ class MainActivity : AppCompatActivity() {
             setOnClickListener { v: View ->
                 val curIndex = editText.selectionStart
                 var passwordStr = editText.text.toString()
-                var passwordLendth = passwordStr.length
+                var passWordLendth = passwordStr.length
 
-                if(curIndex == 0 || passwordLendth == 0) {
+                if(curIndex == 0 || passWordLendth == 0) {
                     return@setOnClickListener
                 }
 
                 passwordStr.apply {
-                    passwordStr = substring(0, curIndex - 1) + substring(curIndex, passwordLendth)
+                    passwordStr = substring(0, curIndex - 1) + substring(curIndex, passWordLendth)
                 }
 
                 currentText.apply {
-                    currentText = StringBuilder(toString().substring(0, curIndex - 1) + toString().substring(curIndex, passwordLendth))
+                    currentText = StringBuilder(toString().substring(0, curIndex - 1) + toString().substring(curIndex, passWordLendth))
                 }
 
-                passwordLendth = passwordStr.length
+                passWordLendth = passwordStr.length
                 editText.setText("")
 
-                for(i in 1..passwordLendth) {
+                for(i in 1..passWordLendth) {
                     editText.append("*")
                 }
                 editText.setSelection(curIndex - 1)
@@ -111,71 +112,67 @@ class MainActivity : AppCompatActivity() {
     private fun reOrderKeyboard(btnWidth: Int) {
         val keyNumberArr = ArrayList<String>()
 
-        for(i in 0..9) {
+        for (i in 0..9) {
             keyNumberArr.add(i.toString())
         }
 
         var tr: TableRow? = null
         var btn: Button? = null
-        var randIndex: Int = 0;
+        var randIndx: Int = 0;
         var random = Random
 
-        for(i in 0..(tablelayout.childCount - 1)) {
-            tr = tablelayout.getChildAt(i) as TableRow
+        for (i in 0..(tableLayout.childCount-1)) {
+            tr = tableLayout.getChildAt(i) as TableRow
 
-            for(i in 0..(tr.childCount - 1)) {
+            for (i in 0..(tr.childCount-1)) {
                 btn = tr.getChildAt(i) as Button
 
-                if(btn.id == -1) {
-                    randIndex = random.nextInt(keyNumberArr.size)
-                    btn.text = keyNumberArr[randIndex]
+                if (btn.id == -1) {
+                    randIndx = random.nextInt(keyNumberArr.size)
+                    btn.text = keyNumberArr[randIndx]
                     btn.width = btnWidth / 3
-
-                    val keyText = btn.text
-
-                    keyNumberArr.removeIf { x -> x == keyNumberArr[randIndex] }
+                    val keyTxt = btn.text
+                    keyNumberArr.removeIf { x -> x == keyNumberArr[randIndx] }
                     btn.setOnClickListener { v: View ->
                         val curIndex = editText.selectionStart
                         var passwordStr = editText.text.toString()
-                        val passwordLength = passwordStr.length
-
+                        val passWordLength = passwordStr.length
                         passwordStr.apply {
-                            substring(0, curIndex) + keyText + substring(curIndex, passwordLength)
+                            substring(0, curIndex) + keyTxt + substring(curIndex,passWordLength)
                         }
 
-                        currentText.append(keyText)
+                        currentText.append(keyTxt)
                         editText.setText("")
 
-                        for(i in 0 until curIndex) {
+                        for (i in 0 until curIndex) {
                             editText.append("*")
                         }
 
-                        editText.append(keyText)
+                        editText.append(keyTxt)
 
-                        for(i in curIndex + 1 until passwordLength + 1) {
+                        for (i in curIndex + 1 until passWordLength + 1) {
                             editText.append("*")
                         }
 
                         editText.setSelection(curIndex + 1)
-                        mHandler.sendEmptyMessageDelayed(0, 1000)
+                        mHandler.sendEmptyMessageDelayed(0,1000)
                     }
                 }
             }
         }
     }
 
-    private var mHandler = Handler() {
+    private var mHandler = Handler(){
             msg: Message? ->
         val curIndex = editText.selectionStart
         editText.setText("")
 
-        for(i in 0 until curIndex) {
+        for (i in 0 until curIndex) {
             editText.append("*")
         }
 
         editText.setSelection(curIndex)
         false
-
     }
 
     private fun getBtnWidth(): Int {
